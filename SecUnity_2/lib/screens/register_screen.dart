af38190a52fbb0faeba8ec65e9fac521d41cf980
TalorLangnas <118974915+TalorLangnas@users.noreply.github.com>
2024-02-06 15:16:18 +0200

    first

--- SecUnity_2/lib/screens/register_screen.dart
+++ SecUnity_2/lib/screens/register_screen.dart
@@ -6,16 +6,53 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterScreen> {
+
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Register')),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Back to Login'),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            TextField(
+              controller: _confirmPasswordController,
+              decoration: InputDecoration(labelText: 'Confirm Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 24),
+            ElevatedButton(
+              child: Text('Register'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            SizedBox(height: 24),
+            ElevatedButton(
+              child: Text('Back to Login'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
       ),
     );
