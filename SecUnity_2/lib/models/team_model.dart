35896e620810acd445ff30ad9882147beecc9ebd
TalorLangnas <118974915+TalorLangnas@users.noreply.github.com>
2024-02-20 16:41:47 +0200

    up

--- SecUnity_2/lib/models/team_model.dart
+++ SecUnity_2/lib/models/team_model.dart
@@ -4,8 +4,20 @@ class Team {
   final String city;
   final String leaderUid;
   final List<String> members;
+  final List<String> position;
   bool alert = false;
 
   Team(this.uid, this.name, this.city, this.leaderUid, this.members,
-      this.alert);
+      this.position, this.alert);
+
+  toJson() {
+    return {
+      'squad_name': name,
+      'city': city,
+      'leader': leaderUid,
+      'members': members,
+      'position': position,
+      'alert': alert,
+    };
+  }
 }
