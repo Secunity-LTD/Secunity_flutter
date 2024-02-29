3cd548b768c26f70766778dd82e7959fa5e89719
Yogev Ofir <yogev282000@gmail.com>
2024-02-29 15:57:41 +0200

    fixed gmail signup page

--- SecUnity_2/lib/screens/authenticate/sign_up_gmail.dart
+++ SecUnity_2/lib/screens/authenticate/sign_up_gmail.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../../constants/constants.dart';
 import '../../services/auth_service.dart';
 import '../Home/home.dart';
+import '../../constants/signin_style.dart';
+import 'sign_up.dart';
 
 class SignUpGmail extends StatefulWidget {
   final Function toggleView;
@@ -16,7 +18,6 @@ class _SignUpGmailState extends State<SignUpGmail> {
   final _formKey = GlobalKey<FormState>();
   bool loading = false;
   String error = '';
-  String userType = '0';
   String firstName = '';
   String lastName = '';
   String dropdownValue = 'Role';
@@ -25,132 +26,169 @@ class _SignUpGmailState extends State<SignUpGmail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: secondary,
+      backgroundColor: Colors.transparent,
       appBar: AppBar(
-        backgroundColor: primary,
+        backgroundColor: SignStyles.backgroundColor1,
         elevation: 0.0,
-        title: Text('complete details for SecUnity'),
+        title: Text('Sign in to SecUnity'),
         actions: <Widget>[
           TextButton.icon(
             onPressed: () {
-              widget.toggleView();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => SignUp(toggleView: () {})),
+              );
             },
-            style: TextButton.styleFrom(foregroundColor: secondary),
+            style: TextButton.styleFrom(
+              foregroundColor: secondary,
+              textStyle: TextStyle(fontSize: 20.0),
+            ),
             icon: Icon(Icons.person),
             label: Text(
-              'Sign In',
+              'Sign Ip',
               style: TextStyle(color: Colors.black),
             ),
           ),
         ],
       ),
-      body: Form(
-        key: _formKey, // Add this line
-        child: ListView(
-          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-          children: <Widget>[
-            SizedBox(height: 10.0),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'First Name',
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  SignStyles.backgroundColor1,
+                  SignStyles.backgroundColor2,
+                  SignStyles.backgroundColor3,
+                  SignStyles.backgroundColor4,
+                  SignStyles.backgroundColor5,
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
               ),
-              validator: (value) =>
-                  value!.isEmpty ? 'Enter a first name' : null,
-              onChanged: (value) {
-                setState(() => firstName = value);
-              },
             ),
-            SizedBox(height: 10.0),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Last Name',
+          ),
+          Center(
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/Secunity_Background.png"),
+                  fit: BoxFit.none,
+                ),
               ),
-              validator: (value) => value!.isEmpty ? 'Enter a last name' : null,
-              onChanged: (value) {
-                setState(() => lastName = value);
-              },
             ),
-            DropdownButton<String>(
-              value: dropdownValue, // Default value
-              onChanged: (String? newValue) {
-                // Update dropdown value
-                setState(() {
-                  dropdownValue = newValue!;
-                });
-                // Handle any other actions
-                print(newValue);
-              },
-              items: <String>['Role', 'Medic', 'Sniper', 'Negev']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            SizedBox(height: 10.0),
-            DropdownButton<String>(
-              value: dropdownValueTeam, // Default value
-              onChanged: (String? newValue) {
-                // Update dropdown value
-                setState(() {
-                  dropdownValueTeam = newValue!;
-                });
-                // Handle any other actions
-                print(newValue);
-              },
-              items: <String>['Type', 'Team leader', 'Crew member']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            SizedBox(height: 10.0),
-            ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(primary),
-              ),
-              child: Text(
-                'Sign up',
-                style: TextStyle(color: secondary),
-              ),
-              onPressed: () async {
-                if (_formKey.currentState!.validate()) {
-                  setState(() => loading = true);
-                  dynamic result = await _authService.SignUpGmail(
-                      firstName, lastName, dropdownValue, dropdownValueTeam);
-                  if (result == null) {
+          ),
+          Form(
+            key: _formKey, // Add this line
+            child: ListView(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              children: <Widget>[
+                SizedBox(height: 10.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'First Name',
+                  ),
+                  validator: (value) =>
+                      value!.isEmpty ? 'Enter a first name' : null,
+                  onChanged: (value) {
+                    setState(() => firstName = value);
+                  },
+                ),
+                SizedBox(height: 10.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Last Name',
+                  ),
+                  validator: (value) =>
+                      value!.isEmpty ? 'Enter a last name' : null,
+                  onChanged: (value) {
+                    setState(() => lastName = value);
+                  },
+                ),
+                DropdownButton<String>(
+                  value: dropdownValue, // Default value
+                  onChanged: (String? newValue) {
+                    // Update dropdown value
                     setState(() {
-                      error = 'Please provide a valid email';
-                      loading = false;
+                      dropdownValue = newValue!;
                     });
-                  } else if (result == 1) {
-                    setState(() {
-                      error = 'The email is already in use';
-                      loading = false;
-                    });
-                  } else {
-                    // Navigator.pushReplacementNamed(context, '/sign_in');
-                    // Navigate to LeaderScreen after successful sign-in
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Home(),
-                      ),
+                    // Handle any other actions
+                    print(newValue);
+                  },
+                  items: <String>['Role', 'Medic', 'Sniper', 'Negev']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
                     );
-                  }
-                }
-              },
+                  }).toList(),
+                ),
+                SizedBox(height: 10.0),
+                DropdownButton<String>(
+                  value: dropdownValueTeam, // Default value
+                  onChanged: (String? newValue) {
+                    // Update dropdown value
+                    setState(() {
+                      dropdownValueTeam = newValue!;
+                    });
+                    // Handle any other actions
+                    print(newValue);
+                  },
+                  items: <String>['Type', 'Team leader', 'Crew member']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                SizedBox(height: 10.0),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(primary),
+                  ),
+                  child: Text(
+                    'Sign up',
+                    style: TextStyle(color: secondary),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      setState(() => loading = true);
+                      dynamic result = await _authService.SignUpGmail(firstName,
+                          lastName, dropdownValue, dropdownValueTeam);
+                      if (result == null) {
+                        setState(() {
+                          error = 'Please provide a valid email';
+                          loading = false;
+                        });
+                      } else if (result == 1) {
+                        setState(() {
+                          error = 'The email is already in use';
+                          loading = false;
+                        });
+                      } else {
+                        // Navigator.pushReplacementNamed(context, '/sign_in');
+                        // Navigate to LeaderScreen after successful sign-in
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Home(),
+                          ),
+                        );
+                      }
+                    }
+                  },
+                ),
+                SizedBox(height: 12.0),
+                Text(
+                  error,
+                  style: TextStyle(color: errorColor, fontSize: 14.0),
+                )
+              ],
             ),
-            SizedBox(height: 12.0),
-            Text(
-              error,
-              style: TextStyle(color: errorColor, fontSize: 14.0),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
